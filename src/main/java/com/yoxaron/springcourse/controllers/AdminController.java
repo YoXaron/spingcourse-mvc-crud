b12f3619cd602b80d96b31f72aee0787bb0de17f
yoxaron <dumbbeliever@gmail.com>
2023-09-04 13:51:21 +0100

    add dropdown list

---
+++ src/main/java/com/yoxaron/springcourse/controllers/AdminController.java
@@ -0,0 +1,36 @@
+package com.yoxaron.springcourse.controllers;
+
+import com.yoxaron.springcourse.dao.PersonDAO;
+import com.yoxaron.springcourse.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private PersonDAO personDAO;
+
+    @Autowired
+    public AdminController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String adminPage(Model model, @ModelAttribute("person") Person person) {
+        model.addAttribute("people", personDAO.index());
+
+        return "people/adminPage";
+    }
+
+    @PatchMapping("/add")
+    public String makeAdmin(@ModelAttribute("person") Person person) {
+        System.out.println(person.getId());
+        return "redirect:/people";
+    }
+}
